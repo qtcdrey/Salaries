902a7ac39e8a195ad5ef50849d8a5a65320f764d
qtcdrey <andreydelrosario8@gmail.com>
2024-11-14 22:02:23 +0800

    this is not advanced and we need to improve

--- salaries.c
+++ salaries.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    float hour_rate, gross_pay, holding_tax, net_pay;
+    int hour_work;
+    printf("Hourly rate: ");
+    scanf("%f", &hour_rate);
+    printf("Hours worked: ");
+    scanf("%d", &hour_work); 
+    gross_pay = hour_rate*hour_work;
+    holding_tax = gross_pay*.32;
+    net_pay = gross_pay-holding_tax;
+    printf("\nGross pay: %.2f", gross_pay);
+    printf("\nWithholding tax: %.2f", holding_tax);
+    printf("\nNet pat: %.2f", net_pay);
+    return 0;
+}
